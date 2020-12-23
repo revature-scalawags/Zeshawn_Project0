ba72cb565ae124c5b8644d69783e8eb47cac37df
ZPMan10197 <zeshawnmanzoor@yahoo.com>
2020-12-22 23:05:01 -0500

    Commit updated csv

--- legislatorscsvreader/src/main/scala/csv/CSVReader.scala
+++ legislatorscsvreader/src/main/scala/csv/CSVReader.scala
@@ -1,13 +1,29 @@
 import scala.io
-import scala.collection
+import scala.collection.mutable.ArrayBuffer
 
 
 object CSVReader extends App {
-  println("State, Senator")
+  
+  // each row is an array of strings (the columns in the csv file)
+  val rows = ArrayBuffer[Array[String]]()
+
+
+  println("State, Name")
   val file = io.Source.fromFile("legislators-current.csv")
   // Parses the CSV File and skips the first row 
   for(line <- file.getLines.drop(1)){
-    val cols = line.split(",").map(_.trim)
-    println(s"${cols(6)}|${cols(0)}")
+    rows += line.split(",").map(_.trim)
+    
   }
+
+  for (row <- rows) {
+      println(s"${row(6)}|${row(0)}")
+  }
+
+  def using[A <: { def close(): Unit }, B](resource: A)(f: A => B): B =
+    try {
+       f(resource)
+    } finally {
+      resource.close()
+    }
 }
