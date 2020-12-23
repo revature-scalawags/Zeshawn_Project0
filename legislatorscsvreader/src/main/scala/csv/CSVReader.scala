542a7b58c0ae42caaf77f2be6f158d496abbc0e3
ZPMan10197 <zeshawnmanzoor@yahoo.com>
2020-12-23 01:50:49 -0500

    adding print methods

--- legislatorscsvreader/src/main/scala/csv/CSVReader.scala
+++ legislatorscsvreader/src/main/scala/csv/CSVReader.scala
@@ -1,29 +1,75 @@
+package scala
+
 import scala.io
 import scala.collection.mutable.ArrayBuffer
+import org.mongodb.scala._
+import scala.concurrent.Future
+import scala.util.Try
 
 
 object CSVReader extends App {
   
+  
+ // val bufferedSource = io.Source.fromFile("congress.csv").getLines.drop(1).toArray
+  var matrix = readCSV()
+  
+  
+
+  def readCSV() : Array[Array[String]] = {
+  io.Source.fromFile("congress.csv")
+    .getLines.drop(1)
+    .map(_.split(",").map(_.trim.toString))
+    .toArray
+  }
+
+  printNames()
   // each row is an array of strings (the columns in the csv file)
-  val rows = ArrayBuffer[Array[String]]()
+  //val rows = ArrayBuffer[Array[String]]()
+
+  /**
+  def readCSV() : Array[Array[String]] = {
+  io.Source.fromFile("legislators-current.csv")
+    .getLines.drop(1)
+    .map(_.split(",").map(_.trim.toString()))
+    .toArray
+  }
+  val matrix = readCSV()
+  countReps()
+  */
 
 
-  println("State, Name")
-  val file = io.Source.fromFile("legislators-current.csv")
+ 
+  /**
+    val file = io.Source.fromFile("legislators-current.csv")
   // Parses the CSV File and skips the first row 
-  for(line <- file.getLines.drop(1)){
+    for(line <- file.getLines.drop(1)){
     rows += line.split(",").map(_.trim)
-    
+    }
+    */
+
+  
+
+ 
+  
+  // Prints the name of the person and state they represent
+  def printNames(): Unit = {
+    for( i<-0 to 535)
+    {
+      println(matrix(i)(0) + " | " + matrix(i)(5))
+    }
   }
 
-  for (row <- rows) {
-      println(s"${row(6)}|${row(0)}")
+  def countReps(): Unit = {
+    print("LOL")
   }
-
+  
+/**
   def using[A <: { def close(): Unit }, B](resource: A)(f: A => B): B =
     try {
        f(resource)
     } finally {
       resource.close()
     }
+*/
+
 }
